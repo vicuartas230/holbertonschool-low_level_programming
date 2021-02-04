db91df9ea1d2736f32ca96bfe607157e74f45a6f
vicuartas230 <vicuartas230@gmail.com>
2021-02-04 23:52:13 +0000

    Task 4 fixed

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+		if (a != 113 && a != 101)
+		{
+			putchar(a);
+		}
+	putchar('\n');
+	return (0);
+}
