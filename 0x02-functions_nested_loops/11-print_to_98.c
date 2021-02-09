622d137524f0f44f3e5a4b955b62d55e2fe69b97
vicuartas230 <vicuartas230@gmail.com>
2021-02-09 23:22:21 +0000

    Task 11 fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,33 +13,102 @@ void print_to_98(int n)
 	if (n < 98)
 	{
 		int a;
-
 		for (a = n; a <= 98; a++)
 		{
-			printf("%d", a);
-			if (a < 98)
+			if (a < 10)
 			{
-				printf(", ");
+				_putchar(a + '0');
+				if (a < 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((a / 10) + '0');
+				_putchar((a % 10) + '0');
+				if (a < 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 	else if (n == 98)
 	{
-		printf("%d\n", n);
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		_putchar('\n');
+	}
+	else if (n < 0)
+	{
+		int c;
+		for (c = n; c <= 98 ; c++)
+		{
+			if (c < 0)
+			{
+				c = -c;
+			}
+			if (c < 10)
+			{
+				_putchar(c + '0');
+				if (c < 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+				if (c < 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
 	}
 	else
 	{
 		int b;
-
 		for (b = n; b >= 98; b--)
 		{
-			printf("%d", b);
-			if (b > 98)
+			if (b < 10)
 			{
-				printf(", ");
+				_putchar(b + '0');
+				if (b > 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else if (b < 100)
+			{
+				_putchar((b / 10) + '0');
+				_putchar((b % 10) + '0');
+				if (b > 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((b / 100) + '0');
+				_putchar(((b / 10) % 10) + '0');
+				_putchar((b % 10) + '0');
+				if (b > 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
