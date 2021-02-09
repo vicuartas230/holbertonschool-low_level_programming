0769a49602413256ea58a8f5a070feee821e3bb3
vicuartas230 <vicuartas230@gmail.com>
2021-02-09 19:29:03 +0000

    Uploading file of task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * add - This function prints the add of two integers
+ *
+ * @a: Integer to add
+ *
+ * @b: Integer to add
+ *
+ * Return: 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	int suma;
+
+	suma = a + b;
+	return (suma + '0');
+}
