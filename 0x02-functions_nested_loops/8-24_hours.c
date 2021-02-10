07ecb849f10bac1193374c904c0b8971140b7d87
vicuartas230 <vicuartas230@gmail.com>
2021-02-10 02:42:53 +0000

    Task 8 fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,32 +8,19 @@
 
 void jack_bauer(void)
 {
-	int s1, s2, m1, m2;
-
-	for (m2 = '0'; m2 < '3'; m2++)
+	int m, h;
+	
+	for(h = 0; h < 24; h++)
 	{
-
-		for (m1 = '0'; m1 <= '9'; m1++)
+		for (m = 0; m < 60; m++)
 		{
-			if ((m1 + m2) < 102)
-			{
-				for (s2 = '0'; s2 < '6'; s2++)
-				{
-					for (s1 = '0'; s1 <= '9'; s1++)
-					{
-						_putchar(m2);
-						_putchar(m1);
-						_putchar(':');
-						_putchar(s2);
-						_putchar(s1);
-						_putchar('\n');
-					}
-					s1 = '0';
-				}
-				s1 = '0';
-				s2 = '0';
-			}
+			putchar((h / 10) + '0');
+			putchar((h % 10) + '0');
+			putchar(':');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			putchar('\n');
 		}
-		m1 = '0';
+		m = 0;
 	}
 }
