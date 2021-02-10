f79d3d9b49eb3d6717a8aa5f2d8589785359d029
vicuartas230 <vicuartas230@gmail.com>
2021-02-10 21:37:35 +0000

    Header file fixed

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,3 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
 int main(void);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+#endif /* HOLBERTON_H */
