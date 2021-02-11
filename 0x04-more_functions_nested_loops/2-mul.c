77ae0527da767d9ca408bc43d001859b4de905e7
vicuartas230 <vicuartas230@gmail.com>
2021-02-11 21:47:39 +0000

    Uploading file of task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * mul - This function prints the product od two integers
+ *
+ * @a: The character 1
+ *
+ * @b: The character 2
+ *
+ * Return: 0 (Succes)
+ */
+
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+
+	return (m);
+}
