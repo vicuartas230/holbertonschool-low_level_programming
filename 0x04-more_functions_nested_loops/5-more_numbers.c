4fc0c7f42725c304dcdb10fdbdad832656bb7639
vicuartas230 <vicuartas230@gmail.com>
2021-02-11 23:45:03 +0000

    Task 5 fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,20 +10,16 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (b = 0; b <= 10; b++)
+	for (a = 0; a < 10; a++)
 	{
-		for (a = 0; a < 15; a++)
+		for (b = 0; b < 15; b++)
 		{
-			if (a < 10)
+			if (b > 9)
 			{
-				_putchar(a + '0');
+				_putchar((b / 10) + '0');
 			}
-			else
-			{
-				_putchar((a / 10) + '0');
-				_putchar((a % 10) + '0');
-			}
+			_putchar((b % 10) + '0');
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
