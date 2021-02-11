8057dc036453597959a2460ad003ca09d35041c4
vicuartas230 <vicuartas230@gmail.com>
2021-02-11 22:58:18 +0000

    Task 9 fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,35 @@ int main(void)
 	{
 		if (a % 3 == 0 && a % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
+			if (a < 100)
+			{
+				putchar(' ');
+			}
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
+			if (a < 100)
+			{
+				putchar(' ');
+			}
 		}
 		else if (a % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
+			if (a < 100)
+			{
+				putchar(' ');
+			}
 		}
 		else
 		{
-			printf("%d ", a);
+			printf("%d", a);
+			if (a < 100)
+			{
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
