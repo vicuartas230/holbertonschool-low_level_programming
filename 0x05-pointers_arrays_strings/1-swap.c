114bb3fe2ee11390c5671f3a384911a472466a8c
vicuartas230 <vicuartas230@gmail.com>
2021-02-16 02:04:22 +0000

    Uploading file of task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * swap_int - this functions swaps two values
+ *
+ * @a: The integer
+ *
+ * @b: Another integer
+ *
+ * Return: 0 (Success)
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
