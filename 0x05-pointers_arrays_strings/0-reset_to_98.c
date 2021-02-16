69b353ee55055dc9bf7f596628726ffa0f1d103b
vicuartas230 <vicuartas230@gmail.com>
2021-02-16 01:58:10 +0000

    Uploading first files and file of task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - This function changes the value of n
+ *
+ * @n: The value to change
+ *
+ * Return: 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This file contains the inofrmation about the pointers and arrays tasks
