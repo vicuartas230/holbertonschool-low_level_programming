d56d6e19f23c2b77c6383cfe864a4c6ae52abe20
vicuartas230 <vicuartas230@gmail.com>
2021-02-16 18:28:59 +0000

    Uploading file of task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include "2-strlen.c"
+
+/**
+ * print_rev - This function prints a string
+ *
+ * @s: The char to print
+ *
+ * Return: 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+	int a;
+
+	for (a = _strlen(s); a >= 0; a--)
+	{
+		_putchar(s[a - 1]);
+	}
+	_putchar('\n');
+}
