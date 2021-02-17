5433dd36c59ba5880f3233fdccc5e1226ca3cb36
vicuartas230 <vicuartas230@gmail.com>
2021-02-17 04:29:53 +0000

    Uploading file of task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - This function reverses a string
+ *
+ * @s: The character to reverse
+ *
+ * Return: 0 (Success)
+ */
+
+void rev_string(char *s)
+{
+	int i, j, k;
+
+	char x[10];
+
+	j = _strlen(s);
+	j--;
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		*(x + i) = s[j];
+		j--;
+	}
+	for (k = 0; x[k] != '\0'; k++)
+	{
+		*(s + k) = x[k];
+}
+	_putchar('\n');
+}
