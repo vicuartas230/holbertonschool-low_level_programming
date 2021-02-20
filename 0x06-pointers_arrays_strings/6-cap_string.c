be81afe83da83f78569261cbd701eb9207bd0d74
vicuartas230 <vicuartas230@gmail.com>
2021-02-20 00:31:47 +0000

    File of task 6 fixed

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,10 @@ char *cap_string(char *str)
 
 	for (a = 0; str[a] != '\0'; ++a)
 	{
-		if (str[a] == 32 || str[a] == 9 || str[a] == 10 || str[a] == 44 || str[a] == 46 || str[a] == 59 || str[a] == 63 || str[a] == 40 || str[a] == 41 || str[a] == 123 || str[a] == 125 || str[a] == 33 || str[a] == 34)
+		if (str[a] == 32 || str[a] == 9 || str[a] == 10 ||
+		    str[a] == 44 || str[a] == 46 || str[a] == 59 ||
+		    str[a] == 63 || str[a] == 40 || str[a] == 41 ||
+		    str[a] == 123 || str[a] == 125 || str[a] == 33 || str[a] == 34)
 		{
 			if (str[a + 1] >= 'a' && str[a + 1] <= 'z')
 			{
