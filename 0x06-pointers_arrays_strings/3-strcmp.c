9a98ce94701b1dc8a30cc79b1ced84498683dfe0
vicuartas230 <vicuartas230@gmail.com>
2021-02-20 04:40:31 +0000

    File of task 3 fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,18 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int a;
+	int a, e;
 
 	for (a = 0; s1[a] && s2[a] != '\0'; a++)
 	{
 		if (s1[a] != s2[a])
 		{
-			return (s1[a] - s2[a]);
+			e = (s1[a] - s2[a]);
 		}
 		else
 		{
-			return (0);
+			e = (0);
 		}
 	}
-	return (0);
+	return (e);
 }
