4af7410d7358c7023463829f2a7dda5b993cf3e8
vicuartas230 <vicuartas230@gmail.com>
2021-02-23 01:37:08 +0000

    File of task 3 fixed

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,11 +9,12 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int a, b, c = 0;
+	int a, b; 
+	unsigned int c = 0;
 
-	for (a = 0; s[a] != '\0'; a++)
+	for (a = 0; s[a] != '\0' && s[a] != ' '; a++)
 	{
-		for (b = 0; b < a; b++)
+		for (b = 0; accept[b] != '\0'; b++)
 		{
 			if (accept[b] == s[a])
 			{
