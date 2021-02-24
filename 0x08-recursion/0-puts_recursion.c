4263f93fa00dfec4845bfb1c85cec9754b2c6d62
vicuartas230 <vicuartas230@gmail.com>
2021-02-24 18:48:38 +0000

    Uploading first files

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - This function prints a string
+ * @s: The string to print
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This file contains the information about the recursion tasks
