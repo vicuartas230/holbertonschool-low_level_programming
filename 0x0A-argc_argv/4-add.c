776b428928a6e652f02d926f7d57ee6a1bea6e0c
vicuartas230 <vicuartas230@gmail.com>
2021-02-27 17:16:38 +0000

    File of task 4 fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,17 +12,17 @@ int main(int argc, char *argv[])
 {
 	int a, b = 0;
 
-	if (argc == 1)
+	if (argc == 1 || argc == 2)
 	{
 		printf("0\n");
-		return (0);
+		return (1);
 	}
 	for (a = 1; a < argc; a++)
 	{
 		if (*argv[a] < '0' || *argv[a] > '9')
 		{
 			printf("Error\n");
-			return (0);
+			return (1);
 		}
 		b = b + atoi(argv[a]);
 	}
