ffdec78d2842cdce1c7f43aef878c32a19ec4752
vicuartas230 <vicuartas230@gmail.com>
2021-03-02 23:06:42 +0000

    File of task 1 fixed

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,14 +13,14 @@ char *_strdup(char *str)
 	int a, b;
 	char *c;
 
-	if (str[0] == '\0')
+	for (b = 0; str[b] != '\0'; b++)
+	{}
+	c = malloc(b * sizeof(char) + 1);
+	if (c == '\0')
 	{
 		c = NULL;
 		return (c);
 	}
-	for (b = 0; str[b] != '\0'; b++)
-	{}
-	c = malloc(b * sizeof(char) + 1);
 	for (a = 0; a < b; a++)
 	{
 		c[a] = str[a];
