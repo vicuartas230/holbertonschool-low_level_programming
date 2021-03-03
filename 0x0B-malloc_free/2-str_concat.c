1250a7aea9c6f825365c7da74e9c2938a5df0b9a
vicuartas230 <vicuartas230@gmail.com>
2021-03-03 01:43:29 +0000

    File of task 2 fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,10 @@ char *str_concat(char *s1, char *s2)
 	int a, b, c, e, f = 0;
 	char *d;
 
-	if (s1[0] == '\0' && s2[0] == '\0')
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (a = 0; s1[a] != '\0'; a++)
 	{}
 	for (b = 0; s2[b] != '\0'; b++)
