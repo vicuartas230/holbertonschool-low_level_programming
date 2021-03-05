4917ae9f2a27ad6443e7d8aa23856349ec84566c
vicuartas230 <vicuartas230@gmail.com>
2021-03-05 00:23:39 +0000

    File of task 3 fixed

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,29 +14,8 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	arr = _calloc((max - min) + 1, sizeof(int));
+	arr = malloc((max - min) + 1 * sizeof(int));
 	if (arr == NULL)
 		return (NULL);
 	return (arr);
-}
-/**
- * _calloc - This function allocates memory for an array, using malloc.
- * @nmemb: The number of allocations of memory.
- * @size: The number of bytes of the string.
- * Return: Nothing.
- */
-
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	unsigned int a;
-	char *m;
-
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	m = malloc(nmemb * size);
-	if (m == NULL)
-		return (NULL);
-	for (a = 0; a < nmemb * size; a++)
-		m[a] = 0;
-	return (m);
 }
