7ba303c6be8407168bfe6deb728be31371277ee6
vicuartas230 <vicuartas230@gmail.com>
2021-03-09 03:24:25 +0000

    Fixing 1 of task 4 file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,6 +14,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	struct dog *dog_t;
 
 	dog_t = malloc(sizeof(struct dog));
+	if (dog_t == NULL)
+		return (NULL);
 	dog_t->name = name;
 	dog_t->age = age;
 	dog_t->owner = owner;
