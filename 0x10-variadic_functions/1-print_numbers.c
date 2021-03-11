b756f44356faff006ea184f5d9f0bbfe3a458069
vicuartas230 <vicuartas230@gmail.com>
2021-03-11 18:58:22 +0000

    Fixing 4 of task 1 file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,16 +14,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list p;
 
-	if (separator != NULL)
-	{
-		va_start(p, n);
+	va_start(p, n);
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(p, int));
-			if (i != n - 1)
+			if (i != n - 1 && separator != NULL)
 				printf("%s", separator);
 		}
 		printf("\n");
 		va_end(p);
-	}
 }
