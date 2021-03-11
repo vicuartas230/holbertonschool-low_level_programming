2690553bf59de32ca3c0a01104904ecd1cac5efd
vicuartas230 <vicuartas230@gmail.com>
2021-03-11 02:35:49 +0000

    Uploading files of task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point.
+ * @ac: Number of arguments of av.
+ * @av: The arguments taken.
+ * Return: 0 (Success)
+ */
+
+int main(int ac, char *av[])
+{
+	int x, y, res;
+	int (*m)(int a, int b);
+
+	if (ac > 4 || ac < 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	m = get_op_func(av[2]);
+	x = atoi(av[1]);
+	y = atoi(av[3]);
+	res = (*m)(x, y);
+	printf("%d\n", res);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,73 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * op_add - This function returns the add of two integers.
+ * @a: The integer 1.
+ * @b: The integer 2.
+ * Return: The add of a and b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - This function returns the subtraction of two integers.
+ * @a: The integer 1.
+ * @b: The integer 2.
+ * Return: The difference of a and b.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - This function returns the product of two integers.
+ * @a: The integer 1.
+ * @b: The integer 2.
+ * Return: The product of a and b.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - This function returns the division of two integers.
+ * @a: The integer 1.
+ * @b: The integer 2.
+ * Return: The division of a by b.
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - This function returns the remainder of the division of two
+ * integers.
+ * @a: The integer 1.
+ * @b: The integer 2.
+ * Return: The remainder of the division of a by b.
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error");
+		exit(100);
+	}
+	return (a % b);
+}
