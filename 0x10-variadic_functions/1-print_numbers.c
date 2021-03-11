35682aa61219e3066b7a5a7f442d49ef2e9735a0
vicuartas230 <vicuartas230@gmail.com>
2021-03-11 18:34:51 +0000

    Fixing 2 of task 1 file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list p;
 
-	if (separator != NULL)
+	if (separator != NULL && n > 0)
 	{
 		va_start(p, n);
 		for (i = 0; i < n; i++)
