2e3cdddec9e6430f29a41a3696b55ab902ecbd99
vicuartas230 <vicuartas230@gmail.com>
2021-03-13 01:05:10 +0000

    Updating header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,11 +2,29 @@
 #define VARIADIC_H
 
 #include <stdio.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * struct form - This struct define a data type formS.
+ * @s: The argument of the function print_all.
+ * @p: The pointer to functions to print data type.
+ */
+
+typedef struct form
+{
+	char *s;
+	void (*p)();
+} formS;
+
 void print_all(const char * const format, ...);
+void printC(va_list s);
+void printI(va_list s);
+void printS(va_list s);
+void printF(va_list s);
 
 #endif
