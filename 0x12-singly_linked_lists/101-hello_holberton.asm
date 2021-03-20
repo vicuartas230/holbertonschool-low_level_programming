b11a08a8dff7d2e21c46ec9ce9efc1798060de44
vicuartas230 <vicuartas230@gmail.com>
2021-03-20 05:19:00 +0000

    Fixing the characters missing

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,5 @@
 section .data
-	text db "Hello, Holberton" ,10
+	text db "Hello, Holberton",10
 	section .text
 	global _start
 
@@ -7,7 +7,7 @@ _start:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, text
-	mov rdx, 14
+	mov rdx, 17
 	syscall
 
 	mov rax, 60
