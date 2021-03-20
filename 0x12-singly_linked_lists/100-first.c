f5cbe0af902071f5e26ee03a03faec385e39f6cf
vicuartas230 <vicuartas230@gmail.com>
2021-03-20 03:59:32 +0000

    Uploading advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * first - This function prints a text before the main function is executed.
+ * Return: Nothing.
+ */
+
+void first(void) __attribute__ ((constructor));
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
