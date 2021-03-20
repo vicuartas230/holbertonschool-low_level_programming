47ddb7595564a944a5e0e199461b8115e408b650
vicuartas230 <vicuartas230@gmail.com>
2021-03-20 04:18:16 +0000

    Fixing another betty error

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,8 @@
  * Return: Nothing.
  */
 
-void first(void) __attribute__ ((constructor));
 
-void first(void)
+void __attribute__((constructor)) first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
