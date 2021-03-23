40dec71f9d557bc403aaa0dcbd7d9c82b059b176
vicuartas230 <vicuartas230@gmail.com>
2021-03-23 19:26:45 +0000

    Fixing some errors about NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,16 +15,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	aux = *head;
-	while (i < idx - 1)
+	while (aux)
 	{
+		if (idx == i + 1)
+			break;
 		aux = aux->next;
 		i++;
 	}
+	if (!aux)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = aux->next;
 	aux->next = new;
-	return (new);
+	return (aux->next);
 }
