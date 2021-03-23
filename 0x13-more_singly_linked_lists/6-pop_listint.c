ca3eec6e99266db1ac308f3f5ef0114943682200
vicuartas230 <vicuartas230@gmail.com>
2021-03-23 16:42:13 +0000

    Fixing case NULL

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,10 +11,14 @@ int pop_listint(listint_t **head)
 	listint_t *aux;
 	int data;
 
+	if (*head)
+	{
 		aux = *head;
 		data = aux->n;
 		aux = aux->next;
 		free(*head);
 		*head = aux;
 		return (data);
+	}
+	return (-1);
 }
