2fb9e8b3f74ebcab98f19db7bfe39f93ae3688f2
vicuartas230 <vicuartas230@gmail.com>
2021-03-26 19:27:30 +0000

    Fixing some printing errors

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,11 +10,11 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int b[1024], *p, in;
+	int b[1024], *p;
 	unsigned int i = 0;
 
-	in = dectobin(n, b);
-	p = &(b[in]);
+	dectobin(n, b);
+	p = &(b[1023]);
 	while (*p != 2)
 	{
 		if (i == index)
@@ -22,10 +22,10 @@ int set_bit(unsigned long int *n, unsigned int index)
 			*(p) = *p | 1;
 			break;
 		}
-		p++;
+		p--;
 		i++;
 	}
-	bintodec(b, n, in);
+	bintodec(b, n);
 	return (1);
 }
 
@@ -36,11 +36,17 @@ int set_bit(unsigned long int *n, unsigned int index)
  * Return: The last position used in the buffer.
  */
 
-int dectobin(unsigned long int *n, int *b)
+void dectobin(unsigned long int *n, int *b)
 {
-	int i = 1023;
+	int i = 0;
 
 	*(b + i) = 2;
+	i++;
+	while (i < 1024)
+	{
+		*(b + i) = 0;
+		i++;
+	}
 	i--;
 	while (*(n) > 0)
 	{
@@ -48,7 +54,7 @@ int dectobin(unsigned long int *n, int *b)
 		*(n) = *n >> 1;
 		i--;
 	}
-	return (i + 1);
+/*	return (i - 1);*/
 }
 
 /**
@@ -59,53 +65,30 @@ int dectobin(unsigned long int *n, int *b)
  * Return: Nothing.
  */
 
-void bintodec(int *b, unsigned long int *n, int pos)
+void bintodec(int *b, unsigned long int *n)
 {
-	int p, i = 0, *ptr;
-	unsigned int ac = 0;
+	int i = 0, *ptr;
+	unsigned long int ac = 0;
 
-	ptr = &(b[pos]);
+/*	ptr = &(b[pos]);
 	while (*(ptr) != 2)
 	{
-		i++;
 		ptr++;
-	}
-	if (i == 1 && *b <= 1)
-		p = *(ptr);
-	else
-		p = _pow(i);
-	ptr = &(b[pos]);
+		}*/
+	ptr = &(b[1023]);
 	while (*(ptr) != 2)
 	{
 		if (*(ptr) == 1)
 		{
-			ac += p;
-			ptr++;
-			p /= 2;
+			ac += 1 << i;
+			ptr--;
+			i++;
 		}
 		else
 		{
-			p /= 2;
-			ptr++;
+			i++;
+			ptr--;
 		}
 	}
 	*(n) = ac;
-}
-
-/**
- * _pow - This function returns the power of a number base 2.
- * @a: The power with base 2 to operate.
- * Return: The result of the operation.
- */
-
-int _pow(int a)
-{
-	int i = 1, res = 1;
-
-	while (i < a)
-	{
-		res *= 2;
-		i++;
-	}
-	return (res);
 }
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -6,12 +6,11 @@
 #include <unistd.h>
 
 unsigned int binary_to_uint(const char *b);
-unsigned long _pow(unsigned long a);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
-int dectobin(unsigned long int *n, int *b);
-void bintodec(int *b, unsigned long int *n, int pos);
+void dectobin(unsigned long int *n, int *b);
+void bintodec(int *b, unsigned long int *n);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _putchar(char c);
