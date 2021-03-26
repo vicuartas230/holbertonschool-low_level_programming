ae5ef22058f4c13e99fca5b30441295aebf5773a
vicuartas230 <vicuartas230@gmail.com>
2021-03-26 21:05:30 +0000

    Fixing for large numbers case

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,11 +10,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int b[2048], *p;
-	unsigned int i = 0;
+	unsigned long int b[4096], *p, i = 0;
 
 	dectobin(n, b);
-	p = &(b[2047]);
+	p = &(b[4095]);
 	while (*p != 2)
 	{
 		if (i == index)
@@ -36,13 +35,13 @@ int set_bit(unsigned long int *n, unsigned int index)
  * Return: The last position used in the buffer.
  */
 
-void dectobin(unsigned long int *n, int *b)
+void dectobin(unsigned long int *n, unsigned long int *b)
 {
-	int i = 0;
+	unsigned long int i = 0;
 
 	*(b + i) = 2;
 	i++;
-	while (i < 2048)
+	while (i < 4096)
 	{
 		*(b + i) = 0;
 		i++;
@@ -63,12 +62,11 @@ void dectobin(unsigned long int *n, int *b)
  * Return: Nothing.
  */
 
-void bintodec(int *b, unsigned long int *n)
+void bintodec(unsigned long int *b, unsigned long int *n)
 {
-	int i = 0, *ptr;
-	unsigned long int ac = 0;
+	unsigned long int i = 0, *ptr, ac = 0;
 
-	ptr = &(b[2047]);
+	ptr = &(b[4095]);
 	while (*(ptr) != 2)
 	{
 		if (*(ptr) == 1)
