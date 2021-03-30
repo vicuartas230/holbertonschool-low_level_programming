38239dc2069f0c69fb91c647a8829daf3438c1b0
vicuartas230 <vicuartas230@gmail.com>
2021-03-30 01:03:18 +0000

    Updating header file

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -6,6 +6,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <limits.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
