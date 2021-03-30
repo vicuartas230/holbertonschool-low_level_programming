1e841c491426992ebac145f7f7940940d6af29ec
vicuartas230 <vicuartas230@gmail.com>
2021-03-30 03:17:57 +0000

    Adding strlen function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,18 +9,16 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t f_d, i, fdw;
+	ssize_t f_d, fdw;
 
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	if (!filename || i > SSIZE_MAX)
+	if (!filename)
 		return (-1);
-	f_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	f_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (f_d == -1)
 		return (-1);
 	if (text_content)
 	{
-		fdw = write(f_d, text_content, i);
+		fdw = write(f_d, text_content, strlen(text_content));
 		if (fdw == -1)
 			return (-1);
 	}
