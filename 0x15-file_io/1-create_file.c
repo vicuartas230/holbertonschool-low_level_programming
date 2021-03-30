8ad9e32c3467db0c4a2921912480dc42cf3f2be8
vicuartas230 <vicuartas230@gmail.com>
2021-03-30 03:16:14 +0000

    Fixing permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		;
 	if (!filename || i > SSIZE_MAX)
 		return (-1);
-	f_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	f_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (f_d == -1)
 		return (-1);
 	if (text_content)
