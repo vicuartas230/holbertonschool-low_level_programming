219bb5583b2b4a477774af7cf1b40787670e48f7
vicuartas230 <vicuartas230@gmail.com>
2021-03-30 17:35:33 +0000

    Uploading task 3 file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point.
+ * @ac: The number of arguments.
+ * @av: The arguments like string.
+ * Return: 0 (Success)
+ */
+
+int main(int ac, char **av)
+{
+	int f, s, rd, wr, c;
+	char *b[1024];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	f = open(av[1], O_RDONLY);
+	if (f == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	rd = read(f, b, 1023);
+	if (rd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	c = close(f);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", f);
+		exit(100);
+	}
+	s = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (s == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	wr = write(s, b, rd);
+	if (wr == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	close(s);
+	if (s == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", s);
+		exit(100);
+	}
+	return (0);
+}
