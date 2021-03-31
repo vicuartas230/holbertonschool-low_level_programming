6ca5bee161b60f36c792a01bd9628248211c5507
vicuartas230 <vicuartas230@gmail.com>
2021-03-31 03:16:35 +0000

    Changing all struct of task 3 file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,69 +1,6 @@
 #include "holberton.h"
 
 /**
- * read_file - This function opens and reads a file.
- * @filename: The name of the file to open.
- * @buffer: The memory space to storage the characters.
- * Return: The number of letter read.
- */
-
-ssize_t read_file(const char *filename, char *buffer)
-{
-	int f;
-	ssize_t rd;
-
-	f = open(filename, O_RDONLY);
-	if (f == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-	rd = read(f, buffer, SSIZE_MAX);
-	if (rd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-	if (close(f) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close %d\n", f);
-		exit(100);
-	}
-	return (rd);
-}
-
-/**
- * write_file - This function writes into a file what is stored in a buffer.
- * @filename: The name of the file to write.
- * @buffer: The memory space where is the text.
- * @rd: The number of characters read.
- * Return: The number of characteres written.
- */
-
-void write_file(const char *filename, char *buffer, ssize_t rd)
-{
-	int s, wr;
-
-	s = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (s == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
-	wr = write(s, buffer, rd);
-	if (wr == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
-	if (close(s) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", s);
-		exit(100);
-	}
-}
-
-/**
  * main - Entry point.
  * @ac: The number of arguments.
  * @av: The arguments like string.
@@ -72,7 +9,7 @@ void write_file(const char *filename, char *buffer, ssize_t rd)
 
 int main(int ac, char **av)
 {
-	int rd;
+	int rd, c_read, c_write, s, f, wr;
 	char b[1024];
 
 	if (ac != 3)
@@ -80,7 +17,29 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	rd = read_file(av[1], b);
-	write_file(av[2], b, rd);
+	f = open(av[1], O_RDONLY);
+	if (f == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]),
+		exit(98);
+	rd = read(f, b, INT_MAX);
+	if (rd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]),
+		exit(98);
+	c_read = close(f);
+	if (c_read == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", f),
+		exit(100);
+	s = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (s == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]),
+		exit(99);
+	wr = write(s, b, rd);
+	if (wr == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]),
+		exit(99);
+	c_write = close(s);
+	if (c_write == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", s),
+		exit(100);
 	return (0);
 }
