857c26d2e0d6fd8d3558af90f5722794b6fb47f1
vicuartas230 <vicuartas230@gmail.com>
2021-03-31 04:31:13 +0000

    Fixing read errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,7 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]),
 		exit(98);
 	rd = read(f, b, BUFSIZ);
-	if (rd == -1)
+	if (rd < 0)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]),
 			close(f),
 		exit(98);
