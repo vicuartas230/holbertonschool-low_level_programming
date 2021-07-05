0bb8d1cb516772eecd9e9c6ae33b38146ce9f322
vicuartas230 <vicuartas230@gmail.com>
2021-07-05 02:57:30 +0000

    Finishing the code

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,8 +5,17 @@
 def island_perimeter(grid):
     """ This function that returns the perimeter
         of the island described in grid """
-    for main_list in grid:
-        if 1 not in main_list:
-            continue
-        for item in main_list:
-            if item and
+    perimeter = 0
+    for main_list in range(len(grid)):
+        """ if 1 not in grid(main_list):
+            continue """
+        for i in range(len(grid[main_list])):
+            if grid[main_list][i] and not grid[main_list][i - 1]:
+                perimeter += 1
+            if grid[main_list][i] and not grid[main_list][i + 1]:
+                perimeter += 1
+            if grid[main_list][i] and not grid[main_list - 1][i]:
+                perimeter += 1
+            if grid[main_list][i] and not grid[main_list + 1][i]:
+                perimeter += 1
+    return perimeter
