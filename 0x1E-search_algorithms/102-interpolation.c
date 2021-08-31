b2447ccd3287afc587db78aacfc540cfe437b6e2
victor <vicuartas230@gmail.com>
2021-08-31 14:21:31 -0500

    Change the formula in all code

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,30 +11,31 @@
 
 int interpolation_search(int *array, size_t size, int value)
 {
-	int position = 0, low = 0, high = size - 1;
+	int low = 0, high = size - 1;
+	size_t position = 0;
 
 	if (!array)
 		return (-1);
 	position = low + (((double)(high - low)) /
 	(array[high] - array[low]) * (value - array[low]));
-	while (position < (int)size)
+	while (position < size)
 	{
-		printf("Value checked array[%d] = [%d]\n", position, array[position]);
+		printf("Value checked array[%ld] = [%d]\n", position, array[position]);
 		if (value == array[position])
 			return (position);
-		if (array[position] > value)
-		{
-			low = position + 1;
-			position = low + (((value - array[low]) *
-			(high - low)) / (array[high] - array[low]));
-		}
 		if (array[position] < value)
 		{
-			high = position - 1;
-			position = low + (((value - array[low]) *
-			(high - low)) / (array[high] - array[low]));
+			low = position - 1;
+			position = low + (((double)(high - low)) /
+			(array[high] - array[low]) * (value - array[low]));
+		}
+		if (array[position] > value)
+		{
+			high = position + 1;
+			position = low + (((double)(high - low)) /
+			(array[high] - array[low]) * (value - array[low]));
 		}
 	}
-	printf("Value checked array[%d] is out of range\n", position);
+	printf("Value checked array[%ld] is out of range\n", position);
 	return (-1);
 }
