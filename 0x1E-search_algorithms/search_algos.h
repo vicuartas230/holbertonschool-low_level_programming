b1fd0dbb2c0914a9b0ec88813fe3bef433d4ed91
victor <vicuartas230@gmail.com>
2021-08-31 16:20:56 -0500

    Update header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,5 +13,6 @@ int second_linear_search(int *array, int low,
 int high, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
