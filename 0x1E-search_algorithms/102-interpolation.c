0d0b2631c0f7154c1341776f940b9215ae8109dd
victor <vicuartas230@gmail.com>
2021-08-31 14:01:13 -0500

    Fix style error

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -15,7 +15,8 @@ int interpolation_search(int *array, size_t size, int value)
 
 	if (!array)
 		return (-1);
-	position = low + (((double)(high - low)) / (array[high] - array[low]) * (value - array[low]));
+	position = low + (((double)(high - low)) /
+	(array[high] - array[low]) * (value - array[low]));
 	while (position < (int)size)
 	{
 		printf("Value checked array[%d] = [%d]\n", position, array[position]);
